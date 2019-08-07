b93fbbd256366289ada896640b1d23e4e102668c
chankaran2996 <45528864+chankaran2996@users.noreply.github.com>
2019-08-07 17:57:15 +0530

    Add files via upload

---
+++ bgsol4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+	char ch;
+	cin>>ch;
+	if((ch>='a'&& ch<='z')||(ch>='a'&& ch<='z'))
+	{
+		cout<<"Alphabet";
+	}
+	else
+	{
+		cout<<"No";
+	}
+	return 0;
+}
